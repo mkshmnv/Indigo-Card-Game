a950a1555945f0a8995937754317fa08f4a476a0
MyIDDQD <nickkosh@gmail.com>
2023-01-26 11:29:50 +0100

    Done Stage 1/5: Card deck symbols

--- Indigo Card Game/task/src/indigo/Main.kt
+++ Indigo Card Game/task/src/indigo/Main.kt
@@ -1,5 +1,13 @@
 package indigo
 
 fun main() {
-    println("Hello World!")
+    println(
+        """
+            A 2 3 4 5 6 7 8 9 10 J Q K
+
+            ♦ ♥ ♠ ♣
+
+            A♠ 2♠ 3♠ 4♠ 5♠ 6♠ 7♠ 8♠ 9♠ 10♠ J♠ Q♠ K♠ A♥ 2♥ 3♥ 4♥ 5♥ 6♥ 7♥ 8♥ 9♥ 10♥ J♥ Q♥ K♥ A♦ 2♦ 3♦ 4♦ 5♦ 6♦ 7♦ 8♦ 9♦ 10♦ J♦ Q♦ K♦ A♣ 2♣ 3♣ 4♣ 5♣ 6♣ 7♣ 8♣ 9♣ 10♣ J♣ Q♣ K♣
+        """.trimIndent()
+    )
 }
